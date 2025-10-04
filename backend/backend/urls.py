ec36e2a5b8d6d91198ae5a9716ee0a54ebcca899
Koded0214h <coder0214h@gmail.com>
2025-10-04 23:19:22 +0100

    backend fix: Updated SpectacularSwaggerView to root URL

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -32,6 +32,6 @@ urlpatterns = [
     
     # OpenAPI schema & docs
     path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("api/v1/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/v1/cleredoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
